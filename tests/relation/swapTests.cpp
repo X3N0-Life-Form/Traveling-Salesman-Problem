369060253192a77834004f8192218d5be3b189c0
X3N0-Life-Form <adrarsis@laposte.net>
2014-04-30 12:20:02 +0200

    use pointers, it's more intuitive

--- tests/relation/swapTests.cpp
+++ tests/relation/swapTests.cpp
@@ -23,7 +23,7 @@ BestFit bf(50000, 280);
 WorstFit wf(50000, 280);
 
 Problem p = parseProblem(path_a280);
-Neighborhood n(p);
+Neighborhood* n;
 
 Swap swap_ff(p, ff);
 Swap swap_bf(p, bf);
@@ -36,45 +36,50 @@ swapTests::~swapTests() {
 }
 
 void swapTests::setUp() {
-    n.generateRandomNeighborhood();
-    n.calculateCost();
-    ff.setInitialCost(n.getCost());
-    bf = BestFit(n.getCost(), 280);
-    wf = WorstFit(n.getCost(), 280);
+    n = new Neighborhood(p);
+    n->generateRandomNeighborhood();
+    n->calculateCost();
+    ff.setInitialCost(n->getCost());
+    bf = BestFit(n->getCost(), 280);
+    wf = WorstFit(n->getCost(), 280);
 }
 
 void swapTests::tearDown() {
+    delete(n);
 }
 
 void swapTests::test_applyRelation_firstFit() {
-    Neighborhood& result = swap_ff.applyRelation(n);
+    Neighborhood* result = swap_ff.applyRelation(*n);
     //std::cout <<"\n"<<result.getCost() << " < "<<n.getCost()<<"\n";
     int result_cost, original_cost;
-    result_cost = result.getCost();
-    original_cost = n.getCost();
+    result_cost = result->getCost();
+    original_cost = n->getCost();
     //CPPUNIT_ASSERT(result.getCost() < n.getCost());
     CPPUNIT_ASSERT(result_cost < original_cost);
+    CPPUNIT_ASSERT(*n != *result);
 }
 
 void swapTests::test_applyRelation_bestFit() {
-    Neighborhood& result = swap_bf.applyRelation(n);
+    Neighborhood* result = swap_bf.applyRelation(*n);
     int result_cost, original_cost, r2_cost;
-    result_cost = result.getCost();
-    original_cost = n.getCost();
+    result_cost = result->getCost();
+    original_cost = n->getCost();
     CPPUNIT_ASSERT(result_cost < original_cost);
-    Neighborhood& r2 = swap_bf.applyRelation(n);
-    r2_cost = r2.getCost();
+    Neighborhood* r2 = swap_bf.applyRelation(*n);
+    r2_cost = r2->getCost();
     CPPUNIT_ASSERT_EQUAL(result_cost, r2_cost);
+    CPPUNIT_ASSERT(*n != *result);
 }
 
 // copy pasted again
 void swapTests::test_applyRelation_worstFit() {
-    Neighborhood& result = swap_wf.applyRelation(n);
+    Neighborhood* result = swap_wf.applyRelation(*n);
     int result_cost, original_cost, r2_cost;
-    result_cost = result.getCost();
-    original_cost = n.getCost();
+    result_cost = result->getCost();
+    original_cost = n->getCost();
     CPPUNIT_ASSERT(result_cost < original_cost);
-    Neighborhood& r2 = swap_wf.applyRelation(n);
-    r2_cost = r2.getCost();
+    Neighborhood* r2 = swap_wf.applyRelation(*n);
+    r2_cost = r2->getCost();
     CPPUNIT_ASSERT_EQUAL(result_cost, r2_cost);
+    CPPUNIT_ASSERT(*n != *result);
 }
