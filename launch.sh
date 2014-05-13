ae062b33460295a8792ccab842d1e3d1755dadef
X3N0-Life-Form <adrarsis@laposte.net>
2014-05-13 10:56:32 +0200

    launch.sh: commented out stuff that doesn't work + compressed lines

--- launch.sh
+++ launch.sh
@@ -9,84 +9,39 @@
 
 ./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
     -file data/a280.tsp \
-    -maxDepth 2000 \
-    -r swap \
-    -s firstFit \
-    -s bestFit \
-    -s worstFit \
-    -o auto
+    -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit -o auto
 
 ./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
     -file data/att48.tsp \
-    -maxDepth 2000 \
-    -r swap \
-    -s firstFit \
-    -s bestFit \
-    -s worstFit \
-    -o auto
+    -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit -o auto
 
 ./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
     -file data/berlin52.tsp \
-    -maxDepth 2000 \
-    -r swap \
-    -s firstFit \
-    -s bestFit \
-    -s worstFit \
-    -o auto
+    -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit -o auto
 
 ./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
     -file data/ali535.tsp \
-    -maxDepth 2000 \
-    -r swap \
-    -s firstFit \
-    -s bestFit \
-    -s worstFit \
-    -o auto
+    -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit -o auto
 
 ./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
     -file data/ch130.tsp \
-    -maxDepth 2000 \
-    -r swap \
-    -s firstFit \
-    -s bestFit \
-    -s worstFit \
-    -o auto
+    -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit -o auto
 
 ./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
     -file data/ch150.tsp \
-    -maxDepth 2000 \
-    -r swap \
-    -s firstFit \
-    -s bestFit \
-    -s worstFit \
-    -o auto
+    -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit -o auto
 
 ./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
     -file data/bier127.tsp \
-    -maxDepth 2000 \
-    -r swap \
-    -s firstFit \
-    -s bestFit \
-    -s worstFit \
-    -o auto
+    -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit -o auto
 
-./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
-    -file data/burma14.tsp \
-    -maxDepth 2000 \
-    -r swap \
-    -s firstFit \
-    -s bestFit \
-    -s worstFit \
-    -o auto
+#./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
+#    -file data/burma14.tsp \
+#    -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit -o auto
 
-./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
-    -file data/brd14051.tsp \
-    -maxDepth 2000 \
-    -r swap \
-    -s firstFit \
-    -s bestFit \
-    -s worstFit \
-    -o auto
+#./dist/Debug/GNU-Linux-x86/traveling_salesman_problem \
+#    -file data/brd14051.tsp \
+#    -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit -o auto
 
 echo "Merging all .csv into allResults.csv"
 cat data/results/*.csv > data/allResults.csv
