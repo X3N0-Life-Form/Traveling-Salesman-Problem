4440f3d24a01f422348cb21fe334ca1a29499690
X3N0-Life-Form <adrarsis@laposte.net>
2014-05-19 14:25:00 +0200

    launch.sh: added make anti-derp safety + restored insert stuff

--- launch.sh
+++ launch.sh
@@ -5,25 +5,27 @@
 #
 # Created on 6 mai 2014, 10:35:01
 #
-#./dist/Debug/GNU-Linux-x86/traveling_salesman_problem -file data/a280.tsp -maxDepth 2000 -r swap -s firstFit -s bestFit -s worstFit
+#./dist/Debug/GNU-Linux-x86/traveling_salesman_problem -file data/a280.tsp -maxDepth 2000 -r swap -r insert -s firstFit -s bestFit -s worstFit
 
 maxDepth=2000
-relations="-r swap"
+relations="-r swap -r insert"
 strategies="-s firstFit -s bestFit -s worstFit"
 
 execPath="./dist/Debug/GNU-Linux-x86/traveling_salesman_problem"
 args="-maxDepth $maxDepth $relations $strategies -o auto"
 
+make
+
 $execPath -file data/a280.tsp $args
 $execPath -file data/att48.tsp $args
 $execPath -file data/berlin52.tsp $args
-$execPath -file data/ali535.tsp $args      #malloc error w/ insert
+#$execPath -file data/ali535.tsp $args      #malloc error w/ insert
 $execPath -file data/ch130.tsp $args
 $execPath -file data/ch150.tsp $args
-$execPath -file data/bier127.tsp $args     #malloc error w/ insert
+#$execPath -file data/bier127.tsp $args     #malloc error w/ insert
 #$execPath -file data/burma14.tsp $args     #parsing error
 #big
-$execPath -file data/brd14051.tsp -maxDepth 5 $relations $strategies -o auto
+$execPath -file data/brd14051.tsp -maxDepth 4 $relations $strategies -o auto
 
 
 echo "Merging all .csv into allResults.csv"
