bd3016c718c3990df89e7dd0c79e2232f4527779
X3N0-Life-Form <adrarsis@laposte.net>
2014-05-23 11:47:46 +0200

    *drumroll*: insert function fix 	added better handling of useless insert requests (origin = target or target is right after origin) 	when checking whether we've reached the origin in the source array, USE THE SOURCE ARRAY INDEX!!! (and not the loop index)

--- code/utils.cpp
+++ code/utils.cpp
@@ -1,16 +1,21 @@
 #include "utils.h"
 
 void insert(int* target_array, int* source_array, int size, int origin, int target) {
+    // if this is a stupid request
+    if (origin == target || target == origin + 1) {
+        ARRAY_COPY(target_array, source_array, size);
+        return;
+    }
+    
+    // if it is a sensible request
     int t_i = 0;
     int s_i = 0;
     for (int i = 0; i < size; i++) {
-        if (origin == target || target == origin + 1) {
-            target_array[t_i] = source_array[s_i];
-        } else if (s_i == target) {
-            target_array[t_i]   = source_array[origin];
+        if (s_i == target) {
+            target_array[t_i] = source_array[origin];
             t_i = (t_i + 1) % size;
             target_array[t_i] = source_array[target];
-        } else if (i == origin) {
+        } else if (s_i == origin) {
             s_i = (s_i + 1) % size;
             target_array[t_i] = source_array[s_i];
         } else {
