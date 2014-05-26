8caf2a2f7965fca204f5573bb115840803eea321
X3N0-Life-Form <adrarsis@laposte.net>
2014-05-26 14:28:28 +0200

    removed meaningless test

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -58,7 +58,6 @@ TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
 TESTFILES= \
 	${TESTDIR}/TestFiles/f6 \
 	${TESTDIR}/TestFiles/f5 \
-	${TESTDIR}/TestFiles/f12 \
 	${TESTDIR}/TestFiles/f10 \
 	${TESTDIR}/TestFiles/f9 \
 	${TESTDIR}/TestFiles/f2 \
@@ -181,10 +180,6 @@ ${TESTDIR}/TestFiles/f5: ${TESTDIR}/tests/strategy/firstFitTests.o ${TESTDIR}/te
 	${MKDIR} -p ${TESTDIR}/TestFiles
 	${LINK.cc}   -o ${TESTDIR}/TestFiles/f5 $^ ${LDLIBSOPTIONS} `cppunit-config --libs`   
 
-${TESTDIR}/TestFiles/f12: ${TESTDIR}/tests/insertCostTests.o ${TESTDIR}/tests/insertCostTests_testrunner.o ${OBJECTFILES:%.o=%_nomain.o}
-	${MKDIR} -p ${TESTDIR}/TestFiles
-	${LINK.cc}   -o ${TESTDIR}/TestFiles/f12 $^ ${LDLIBSOPTIONS} `cppunit-config --libs`   
-
 ${TESTDIR}/TestFiles/f10: ${TESTDIR}/tests/relation/InsertTests.o ${TESTDIR}/tests/relation/InsertTests_testrunner.o ${OBJECTFILES:%.o=%_nomain.o}
 	${MKDIR} -p ${TESTDIR}/TestFiles
 	${LINK.cc}   -o ${TESTDIR}/TestFiles/f10 $^ ${LDLIBSOPTIONS} `cppunit-config --libs`   
@@ -244,18 +239,6 @@ ${TESTDIR}/tests/strategy/firstFitTests_testrunner.o: tests/strategy/firstFitTes
 	${MKDIR} -p ${TESTDIR}/tests/strategy
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 `cppunit-config --cflags` -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/strategy/firstFitTests_testrunner.o tests/strategy/firstFitTests_testrunner.cpp
-
-
-${TESTDIR}/tests/insertCostTests.o: tests/insertCostTests.cpp 
-	${MKDIR} -p ${TESTDIR}/tests
-	${RM} "$@.d"
-	$(COMPILE.cc) -g -std=c++11 `cppunit-config --cflags` -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/insertCostTests.o tests/insertCostTests.cpp
-
-
-${TESTDIR}/tests/insertCostTests_testrunner.o: tests/insertCostTests_testrunner.cpp 
-	${MKDIR} -p ${TESTDIR}/tests
-	${RM} "$@.d"
-	$(COMPILE.cc) -g -std=c++11 `cppunit-config --cflags` -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/insertCostTests_testrunner.o tests/insertCostTests_testrunner.cpp
 
 
 ${TESTDIR}/tests/relation/InsertTests.o: tests/relation/InsertTests.cpp 
@@ -567,7 +550,6 @@ ${OBJECTDIR}/code/utils_nomain.o: ${OBJECTDIR}/code/utils.o code/utils.cpp
 	then  \
 	    ${TESTDIR}/TestFiles/f6 || true; \
 	    ${TESTDIR}/TestFiles/f5 || true; \
-	    ${TESTDIR}/TestFiles/f12 || true; \
 	    ${TESTDIR}/TestFiles/f10 || true; \
 	    ${TESTDIR}/TestFiles/f9 || true; \
 	    ${TESTDIR}/TestFiles/f2 || true; \
